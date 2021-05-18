159a2fa7abfcd7efd79343902896db95b7033998
Mihail <41691809+Koallla@users.noreply.github.com>
2021-05-18 21:06:14 +0300

    added prettTable

--- hw4/main.py
+++ hw4/main.py
@@ -3,7 +3,10 @@ from pathlib import Path
 import os
 import sys
 import shutil
+from time import sleep
 from uuid import uuid4
+
+
 
 
 try:
@@ -12,98 +15,45 @@ except ModuleNotFoundError:
     from .helpers import *
 
 
-# try:
-#     path_dir = sys.argv[1]
-# except IndexError:
-#     path_dir = input('Enter path to directory: ')
-
-path_dir = 'D:\Test'
-path = Path(path_dir)
-
-photo = []
-video = []
-docs = []
-music = []
-zip_data = []
-unknown_files = []
-
-
+path = root_path_dir
 
 # Получение всех файлов, в том числе вложенных, 
-def get_files_list(path=Path(path_dir)):
+def get_files_list(path):
 
-    
-    # Перевод всех файлов на латиницу
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(rename_files, [file for file in path.iterdir() if file.is_file()])
+        try:
+            for file in path.iterdir():
+                if file.is_file():
 
-    # for file in path.iterdir():
-    #     rename_files(file)
+                    # Перевод всех файлов на латиницу
+                    with concurrent.futures.ThreadPoolExecutor() as executor:
+                        executor.map(rename_files, [file])
+        except FileNotFoundError:
+            print('You entered wrong path! Please, try again!')
+            return
 
-
-
-# Вынести всю логику в функцию remove_files()
-    for file in path.iterdir():
-        if file.is_file():
-
-            ext_lower = file.suffix[1:].lower()
-
-            if ext_lower in (photo_filter):
-                photo.append(file.name)
-                remove_files('Images', file, path_dir)
-                
-            elif ext_lower in (video_filter):
-                video.append(file.name)
-                remove_files('Video', file, path_dir)
-
-            elif ext_lower in (docs_filter):
-                docs.append(file.name)
-                remove_files('Documents', file, path_dir)
-
-            elif ext_lower in (music_filter):
-                music.append(file.name)
-                remove_files('Audio', file, path_dir)
-            
-            
-            elif ext_lower in (zip_data_filter):
-                zip_data.append(file.name)
-                unpack_archive_files(file, path_dir, zip_data)
-            
-
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for file in path.iterdir():
+            if file.is_file():
+                # Перемещение файлов
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    executor.map(moving_files, [file])
+            elif file.is_dir() and file.name in ignore_dir:
+                continue
             else:
-                unknown_files.append(file.name)
-                remove_files('Unknown_files', file, path_dir)
+                # Создаем новый путь
+                path_for_recursion = Path(f'{file.parent}\{file.name}')
 
-                
-    #     elif file.is_dir() and file.name in ignore_dir:
-    #         continue
+                # Рекурсия
+                get_files_list(path_for_recursion)
 
-    #     else:
-    #         # Создаем новый путь
-    #         path_for_recursion = Path(f'{file.parent}\{file.name}')
+                # Удаление пустых директорий
+                try:
+                    Path.rmdir(file)
+                except OSError:
+                    continue
 
-    #         # Рекурсия
-    #         get_files_list(path_for_recursion)
-
-    #         # Удаление пустых директорий
-    #         try:
-    #             Path.rmdir(file)
-    #         except OSError:
-    #             continue
 
 get_files_list(path)
 
-
-
-
-
-
-
-
-
-# if photo or video or docs or music or zip_data:
-#     create_table_string_format('photo', photo)
-#     create_table_string_format('video', video)
-#     create_table_string_format('docs', docs)
-#     create_table_string_format('music', music)
-#     create_table_string_format('zip_data', zip_data)
+show_result()
