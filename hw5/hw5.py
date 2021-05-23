7e9d0a8658449abba869c11d0c8fa91d74975266
Mihail <41691809+Koallla@users.noreply.github.com>
2021-05-23 12:37:01 +0300

    added Pool and ProcessPoolExecutor

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,47 +1,87 @@
-# from time import time
-
-
-# start = time()
-# def factorize(number):
-#     result = []
-#     number_list = [num for num in range(1, number + 1)]
-#     for n in number_list:
-#         if number % n == 0:
-#             result.append(n)
-
-#     return result
-
-# a = factorize(128)
-# b = factorize(255)
-# c = factorize(99999)
-# d = factorize(10651060)
-
-
-
-# assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-# assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-# assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-# assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
-
-# print(round(time() - start))
-
-
 from time import time
 
 
-start = time()
+# start = time()
+def factorize(number):
+    result = []
+    number_list = [num for num in range(1, number + 1)]
+    for n in number_list:
+        if number % n == 0:
+            result.append(n)
 
-def factorize(*number):
-    print(number)
+    return result
 
 
-a, b, c, d  = factorize(128, 255, 99999, 10651060)
+a = factorize(128)
+b = factorize(255)
+c = factorize(99999)
+d = factorize(10651060)
 
 
 
-# assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-# assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-# assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-# assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
-print(round(time() - start))
+# print(time() - start)
+
+# =======================================================================================
+# With Pool
+from multiprocessing import Pool
+
+
+# start = time()
+def factorize(number):
+    result = []
+    number_list = [num for num in range(1, number + 1)]
+    for n in number_list:
+        if number % n == 0:
+            result.append(n)
+
+    return result
+
+args = [128, 255, 99999, 10651060]
+
+if __name__ == '__main__':
+    with Pool() as pool:
+        a, b, c, d = pool.map(factorize, args)
+
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
+
+# print(time() - start)
+
+# =================================================================================================
+# With ProcessPoolExecutor
+
+import concurrent.futures
+
+# start = time()
+def factorize(number):
+    result = []
+    number_list = [num for num in range(1, number + 1)]
+    for n in number_list:
+        if number % n == 0:
+            result.append(n)
+
+    return result
+
+args = [128, 255, 99999, 10651060]
+
+if __name__ == '__main__':
+    with concurrent.futures.ProcessPoolExecutor(4) as executor:
+        a, b, c, d = executor.map(factorize, args)
+
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
+
+# print(time() - start)
